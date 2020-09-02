7d7d0f2af7c063b6028c32eac53cbe2d3b3e9f48
Andreas Sagen <andreassag@hotmail.no>
2020-09-02 20:33:42 +0200

    Added c-algorithms subdirectory with conversions for binary and decimal

---
+++ c-algorithms/conversions/binary_to_decimal.c
@@ -0,0 +1,65 @@
+/* binary_to_decimal.c */
+#include <stdbool.h>
+#include <assert.h>
+
+bool is_binary(int);
+int bin2dec(int);
+int test_bin2dec(void);
+
+int main(int argc, char *argv[])
+{
+  // TODO: Implement script interaction.
+}
+
+// TODO: Make function standalon for reusability
+bool is_binary(int binary_number)
+{
+  int reminder = 0, temp;
+  while (binary_number > 0)
+  {
+    reminder = binary_number % 10;
+
+    if ((reminder != 0) && (reminder != 1))
+    {
+      return false;
+    }
+
+    binary_number /= 10;
+  }
+  return true;
+}
+
+int bin2dec(int binary_number)
+{
+  if (!is_binary(binary_number))
+  {
+    return 0;
+  }
+
+  int remainder, decimal_number = 0, temp = 1;
+
+  while (binary_number > 0)
+  {
+    remainder = binary_number % 10;
+    binary_number /= 10;
+    decimal_number += remainder * temp;
+    temp *= 2;
+  }
+  return decimal_number;
+}
+
+int test_bin2dec(void)
+{
+  int n1 = 1010, n2 = 1, n3 = 1234, n4 = 0, n5 = 11111, n6 = 111;
+
+  assert(is_binary(n1) == true);
+  assert(bin2dec(n1) == 10);
+  assert(is_binary(n2) == true);
+  assert(bin2dec(n2) == 1);
+  assert(is_binary(n3) == false);
+  assert(bin2dec(n3) == 0);
+  assert(is_binary(n4) == true);
+  assert(bin2dec(n4) == 0);
+  assert(bin2dec(n5) == 31);
+  assert(bin2dec(n6) == 7);
+}
---
+++ c-algorithms/conversions/decimal_to_binary.c
@@ -0,0 +1,40 @@
+/* decimal_to_binary.c */
+#include <assert.h>
+
+int dec2bin(int);
+int test_dec2bin(void);
+
+int main(int argc, char *argv[])
+{
+  // TODO: Implement script interaction.
+  test_dec2bin();
+}
+
+int dec2bin(int decimal_number)
+{
+  int remainder, binary_number = 0, temp = 1;
+
+  while (decimal_number > 0)
+  {
+    remainder = decimal_number % 2;
+    //printf("%d\n", remainder);
+    decimal_number /= 2;
+    binary_number += remainder * temp;
+    //printf("%d\n", remainder * temp);
+    temp *= 10;
+  }
+
+  return binary_number;
+}
+
+int test_dec2bin(void)
+{
+  int n1 = 21, n2 = 1, n3 = 14, n4 = 0, n5 = 1000, n6 = 123;
+
+  assert(dec2bin(n1) == 10101);
+  assert(dec2bin(n2) == 1);
+  assert(dec2bin(n3) == 1110);
+  assert(dec2bin(n4) == 0);
+  assert(dec2bin(n5) == 1111101000);
+  assert(dec2bin(n6) == 1111011);
+}
